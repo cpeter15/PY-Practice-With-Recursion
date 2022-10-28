b655ecacf59eb8b9b4594f6b5d103bed79f4afb2
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-10-27 20:37:29 -0400

    added solutions

--- solution4.py
+++ solution4.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 4 below
+
+def exponents(x, y):
+
+    if (y == 1):
+        return x
+    else:
+        return (x * exponents(x, y-1))
+
+
+print(exponents(3, 4))
--- solution5.py
+++ solution5.py
@@ -1,1 +1,14 @@
 # Write code for algorithm 5 below
+
+def palindrome(str):
+    if (len(str) == 1 or len(str) == 0):
+        return True
+    elif (str[0] != str[-1]):
+        return False
+    else:
+        return palindrome(str[1:-1])
+
+
+print(palindrome("racecar"))
+
+print(palindrome("palindrome"))
